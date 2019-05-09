6a8c4c5983137f3466974cfcb8d6855418aa9866
vn0x42k <vn0x42k@homeoffice.wal-mart.com>
2019-05-09 15:03:10 +0800

    config socket options

--- src/main/java/wildengineer/cassandra/data/copy/CassandraConfiguration.java
+++ src/main/java/wildengineer/cassandra/data/copy/CassandraConfiguration.java
@@ -20,107 +20,115 @@ import java.security.KeyStore;
 @Configuration
 public class CassandraConfiguration {
 
-	@Autowired
-	private SourceCassandraProperties sourceCassandraProperties;
+    @Autowired
+    private SourceCassandraProperties sourceCassandraProperties;
 
-	@Autowired
-	private DestinationCassandraProperties destinationCassandraProperties;
+    @Autowired
+    private DestinationCassandraProperties destinationCassandraProperties;
 
-	private Cluster cluster;
-	private String cassandraHost = "127.0.0.1";
-	private int cassandraPort = 10350;
-	private String cassandraUsername = "localhost";
-	private String cassandraPassword = "defaultpassword";
-	private File sslKeyStoreFile = null;
-	private String sslKeyStorePassword = "changeit";
-	private String keyspace;
+    private Cluster cluster;
+    private String cassandraHost = "127.0.0.1";
+    private int cassandraPort = 10350;
+    private String cassandraUsername = "localhost";
+    private String cassandraPassword = "defaultpassword";
+    private File sslKeyStoreFile = null;
+    private String sslKeyStorePassword = "changeit";
+    private String keyspace;
 
-	@Bean
-	public Session sourceSession() {
-		return buildSession(sourceCassandraProperties);
-	}
+    @Bean
+    public Session sourceSession() {
+        return buildSession(sourceCassandraProperties);
+    }
 
-	@Bean
-	public Session destinationSession() throws Exception{
-		return buildDestinationSession(destinationCassandraProperties);
-	}
+    @Bean
+    public Session destinationSession() throws Exception {
+        return buildDestinationSession(destinationCassandraProperties);
+    }
 
-	private Session buildSession(CassandraProperties cassandraProperties) {
-		//Turning off jmx reporting, since we don't need it and we don't want to bring in any unnecessary dependencies
-		//See https://docs.datastax.com/en/developer/java-driver/3.5/manual/metrics/#metrics-4-compatibility
-		Cluster cluster = Cluster.builder()
-				.addContactPoints(cassandraProperties.getContactPoints().split(","))
-				.withCredentials(cassandraProperties.getUsername(), cassandraProperties.getPassword())
-				.withPort(cassandraProperties.getPort())
-				.withoutJMXReporting()
-				.build();
-		cluster.getConfiguration().getPoolingOptions().setMaxConnectionsPerHost(HostDistance.LOCAL, 64);
-		cluster.getConfiguration().getPoolingOptions().setMaxConnectionsPerHost(HostDistance.REMOTE, 16);
-		return cluster.connect(cassandraProperties.getKeyspace());
-	}
+    private Session buildSession(CassandraProperties cassandraProperties) {
+        //Turning off jmx reporting, since we don't need it and we don't want to bring in any unnecessary dependencies
+        //See https://docs.datastax.com/en/developer/java-driver/3.5/manual/metrics/#metrics-4-compatibility
+        final SocketOptions socketOptions = new SocketOptions();
+        socketOptions.setConnectTimeoutMillis(50000000).setReadTimeoutMillis(120000000);
 
-	private Session buildDestinationSession(CassandraProperties cassandraProperties) throws Exception{
-		loadCassandraConnectionDetails(cassandraProperties);
-		final KeyStore keyStore = KeyStore.getInstance("JKS");
-		try (final InputStream is = new FileInputStream(sslKeyStoreFile)) {
-			keyStore.load(is, sslKeyStorePassword.toCharArray());
-
-		}
-		final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
-				.getDefaultAlgorithm());
-		kmf.init(keyStore, sslKeyStorePassword.toCharArray());
-		final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory
-				.getDefaultAlgorithm());
-		tmf.init(keyStore);
-
-		// Creates a socket factory for HttpsURLConnection using JKS contents.
-		final SSLContext sc = SSLContext.getInstance("TLSv1.2");
-		sc.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new java.security.SecureRandom());
-
-		JdkSSLOptions sslOptions = RemoteEndpointAwareJdkSSLOptions.builder()
-				.withSSLContext(sc)
-				.build();
-		cluster = Cluster.builder()
-				.addContactPoint(cassandraHost)
-				.withPort(cassandraPort)
-				.withCredentials(cassandraUsername, cassandraPassword)
-				.withSSL(sslOptions)
-				.withoutJMXReporting()
-				.build();
+        Cluster cluster = Cluster.builder()
+                .addContactPoints(cassandraProperties.getContactPoints().split(","))
+                .withCredentials(cassandraProperties.getUsername(), cassandraProperties.getPassword())
+                .withPort(cassandraProperties.getPort())
+                .withSocketOptions(socketOptions)
+                .withoutJMXReporting()
+                .build();
         cluster.getConfiguration().getPoolingOptions().setMaxConnectionsPerHost(HostDistance.LOCAL, 64);
         cluster.getConfiguration().getPoolingOptions().setMaxConnectionsPerHost(HostDistance.REMOTE, 16);
-		System.out.println("get keyspace: " + cassandraProperties.getKeyspace());
-		return cluster.connect(cassandraProperties.getKeyspace());
-	}
+        return cluster.connect(cassandraProperties.getKeyspace());
+    }
 
-	private void loadCassandraConnectionDetails(CassandraProperties cassandraProperties) throws Exception {
+    private Session buildDestinationSession(CassandraProperties cassandraProperties) throws Exception {
+        loadCassandraConnectionDetails(cassandraProperties);
+        final KeyStore keyStore = KeyStore.getInstance("JKS");
+        try (final InputStream is = new FileInputStream(sslKeyStoreFile)) {
+            keyStore.load(is, sslKeyStorePassword.toCharArray());
 
-		cassandraHost = cassandraProperties.getContactPoints();
-		cassandraPort = cassandraProperties.getPort();
-		cassandraUsername = cassandraProperties.getUsername();
-		cassandraPassword = cassandraProperties.getPassword();
-		String ssl_keystore_file_path = null;
-		String ssl_keystore_password = null;
+        }
+        final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
+                .getDefaultAlgorithm());
+        kmf.init(keyStore, sslKeyStorePassword.toCharArray());
+        final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory
+                .getDefaultAlgorithm());
+        tmf.init(keyStore);
 
-		// If ssl_keystore_file_path, build the path using JAVA_HOME directory.
-		if (ssl_keystore_file_path == null || ssl_keystore_file_path.isEmpty()) {
-			//String javaHomeDirectory = System.getenv("JAVA_HOME");
-			//String javaHomeDirectory = "C:\\Program Files\\java\\jdk1.8.0_25";
-			String javaHomeDirectory = "/usr/lib/jvm/java-8-openjdk-amd64";
-			if (javaHomeDirectory == null || javaHomeDirectory.isEmpty()) {
-				throw new Exception("JAVA_HOME not set");
-			}
-			ssl_keystore_file_path = new StringBuilder(javaHomeDirectory).append("/jre/lib/security/cacerts").toString();
-			System.out.println("ssl_keystore_file_path: " + ssl_keystore_file_path);
-		}
+        final SocketOptions socketOptions = new SocketOptions();
+        socketOptions.setConnectTimeoutMillis(50000000).setReadTimeoutMillis(120000000);
 
-		sslKeyStorePassword = (ssl_keystore_password != null && !ssl_keystore_password.isEmpty()) ?
-				ssl_keystore_password : sslKeyStorePassword;
+        // Creates a socket factory for HttpsURLConnection using JKS contents.
+        final SSLContext sc = SSLContext.getInstance("TLSv1.2");
+        sc.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new java.security.SecureRandom());
 
-		sslKeyStoreFile = new File(ssl_keystore_file_path);
+        JdkSSLOptions sslOptions = RemoteEndpointAwareJdkSSLOptions.builder()
+                .withSSLContext(sc)
+                .build();
+        cluster = Cluster.builder()
+                .addContactPoint(cassandraHost)
+                .withPort(cassandraPort)
+                .withCredentials(cassandraUsername, cassandraPassword)
+                .withSocketOptions(socketOptions)
+                .withSSL(sslOptions)
+                .withoutJMXReporting()
+                .build();
+        cluster.getConfiguration().getPoolingOptions().setMaxConnectionsPerHost(HostDistance.LOCAL, 64);
+        cluster.getConfiguration().getPoolingOptions().setMaxConnectionsPerHost(HostDistance.REMOTE, 16);
+        System.out.println("get keyspace: " + cassandraProperties.getKeyspace());
+        return cluster.connect(cassandraProperties.getKeyspace());
+    }
 
-		if (!sslKeyStoreFile.exists() || !sslKeyStoreFile.canRead()) {
-			throw new Exception(String.format("Unable to access the SSL Key Store file from %s", ssl_keystore_file_path));
-		}
-	}
+    private void loadCassandraConnectionDetails(CassandraProperties cassandraProperties) throws Exception {
+
+        cassandraHost = cassandraProperties.getContactPoints();
+        cassandraPort = cassandraProperties.getPort();
+        cassandraUsername = cassandraProperties.getUsername();
+        cassandraPassword = cassandraProperties.getPassword();
+        String ssl_keystore_file_path = null;
+        String ssl_keystore_password = null;
+
+        // If ssl_keystore_file_path, build the path using JAVA_HOME directory.
+        if (ssl_keystore_file_path == null || ssl_keystore_file_path.isEmpty()) {
+            //String javaHomeDirectory = System.getenv("JAVA_HOME");
+            //String javaHomeDirectory = "C:\\Program Files\\java\\jdk1.8.0_25";
+            String javaHomeDirectory = "/usr/lib/jvm/java-8-openjdk-amd64";
+            if (javaHomeDirectory == null || javaHomeDirectory.isEmpty()) {
+                throw new Exception("JAVA_HOME not set");
+            }
+            ssl_keystore_file_path = new StringBuilder(javaHomeDirectory).append("/jre/lib/security/cacerts").toString();
+            System.out.println("ssl_keystore_file_path: " + ssl_keystore_file_path);
+        }
+
+        sslKeyStorePassword = (ssl_keystore_password != null && !ssl_keystore_password.isEmpty()) ?
+                ssl_keystore_password : sslKeyStorePassword;
+
+        sslKeyStoreFile = new File(ssl_keystore_file_path);
+
+        if (!sslKeyStoreFile.exists() || !sslKeyStoreFile.canRead()) {
+            throw new Exception(String.format("Unable to access the SSL Key Store file from %s", ssl_keystore_file_path));
+        }
+    }
 }
